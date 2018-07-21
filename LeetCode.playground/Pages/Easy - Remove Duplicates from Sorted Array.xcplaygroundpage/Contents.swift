92fae8eea395a517120081439eee05daf4989a9c
Rakuyo <rakuyo.mo@gmail.com>
2018-07-21 13:51:39 +0800

    简单难度 - 删除排序数组中的重复项

---
+++ LeetCode.playground/Pages/Easy - Remove Duplicates from Sorted Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,93 @@
+//: [Previous](@previous)
+/*:
+ 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ 
+ 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ 
+ 示例 1:
+ 
+ 给定数组 nums = [1,1,2],
+ 
+ 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ 
+ 你不需要考虑数组中超出新长度后面的元素。
+ 示例 2:
+ 
+ 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ 
+ 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ 
+ 你不需要考虑数组中超出新长度后面的元素。
+ 说明:
+ 
+ 为什么返回数值是整数，但输出的答案是数组呢?
+ 
+ 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ 
+ 你可以想象内部操作如下:
+ 
+ // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ int len = removeDuplicates(nums);
+ 
+ // 在函数里修改输入数组对于调用者是可见的。
+ // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ for (int i = 0; i < len; i++) {
+    print(nums[i]);
+ }
+ */
+import Foundation
+
+// 测试用例
+var testOne   = [0,0,1,1,1,2,2,3,3,4]  // [0, 1, 2, 3, 4] -> 5
+var testTwo   = [1,1,2]                // [1, 2] -> 2
+var testThree = [-3,-1,-1,0,0,0,0,0,2] // [-3, -1, 0, 2] -> 4
+
+
+/* ------- ------- ------- ------- ------- ------- ------- ------- ------- ------- ------- */
+
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+
+    var tmpNum: Int? = nil
+    
+    for value in nums {
+        
+        if value != tmpNum { tmpNum = value
+        } else { nums.remove(at: nums.firstIndex(of: value)!) }
+    }
+
+    return nums.count
+}
+
+removeDuplicates(&testOne)      // [0, 1, 2, 3, 4] -> 5
+testOne
+removeDuplicates(&testTwo)      // [1, 2] -> 2
+testTwo
+removeDuplicates(&testThree)    // [-3, -1, 0, 2] -> 4
+testThree
+
+/* ------- ------- ------- ------- ------- ------- ------- ------- ------- */
+/*:
+ **最佳解答**
+ */
+func removeDuplicates2(_ nums: inout [Int]) -> Int {
+    if nums.count == 0 { return 0 }
+    
+    var i = 0
+    for k in 1 ..< nums.count {
+        if nums[i] != nums[k] {
+            i = i + 1
+            nums[i] = nums[k]
+        }
+    }
+    
+    return i + 1
+}
+
+removeDuplicates2(&testOne)      // [0, 1, 2, 3, 4] -> 5
+testOne
+removeDuplicates2(&testTwo)      // [1, 2] -> 2
+testTwo
+removeDuplicates2(&testThree)    // [-3, -1, 0, 2] -> 4
+testThree
+
+//: [Next](@next)
