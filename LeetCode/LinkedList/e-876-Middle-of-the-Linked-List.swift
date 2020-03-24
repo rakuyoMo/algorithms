e619090c82ad4a7d995a8887a8af8f5313d6379f
Rakuyo <rakuyo.mo@gmail.com>
2020-03-24 20:56:39 +0800

    update

--- LeetCode/LinkedList/e-876-Middle-of-the-Linked-List.swift
+++ LeetCode/LinkedList/e-876-Middle-of-the-Linked-List.swift
@@ -17,36 +17,39 @@ class ListNode {
     }
 }
 
-/// 876. 链表的中间结点 简单
-/// 
-/// 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
-///
-/// 如果有两个中间结点，则返回第二个中间结点。
-///
-/// - Parameter head:
-/// - Returns:
-func middleNode(_ head: ListNode?) -> ListNode? {
-    
-    var fast = head
-    var slow = head
-    
-    while fast?.next != nil {
-        slow = slow?.next
-        fast = fast?.next?.next
+class _876 {
+
+    /// 876. 链表的中间结点 简单
+    /// 
+    /// 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+    ///
+    /// 如果有两个中间结点，则返回第二个中间结点。
+    ///
+    /// - Parameter head:
+    /// - Returns:
+    func middleNode(_ head: ListNode?) -> ListNode? {
+
+        var fast = head
+        var slow = head
+
+        while fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+
+        return slow
     }
-    
-    return slow
+
+    func solution() {
+
+        let node = ListNode(3)
+
+        node.next?.val = 4
+        node.next?.next?.val = 5
+        node.next?.next?.next?.val = 6
+
+        print(String(describing: middleNode(node)?.val))
+    }
 }
 
-func solution() {
-    
-    let node = ListNode(3)
-    
-    node.next?.val = 4
-    node.next?.next?.val = 5
-    node.next?.next?.next?.val = 6
-    
-    print(String(describing: middleNode(node)?.val))
-}
-
-solution()
+_876().solution()
