3bb61cc05b115e046227bdb3f531db777a47ace7
Rakuyo <rakuyo.mo@gmail.com>
2020-03-26 17:04:38 +0800

    Create TreeNode

---
+++ LeetCode/TreeNode/TreeNode.swift
@@ -0,0 +1,23 @@
+//
+//  TreeNode.swift
+//  LeetCode
+//
+//  Created by Rakuyo on 2020/3/26.
+//  Copyright © 2020 com.rakuyo. All rights reserved.
+//
+
+import Foundation
+
+/// Definition for a binary tree node.
+class TreeNode {
+    
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+    
+    init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
