50b3b58ac0021450a2283f00babd672364656b4f
Rakuyo <rakuyo.mo@gmail.com>
2020-04-10 17:59:02 +0800

    22

---
+++ LeetCode/String/m-22-Generate-Parentheses.swift
@@ -0,0 +1,70 @@
+//
+//  m-22-Generate-Parentheses.swift
+//  LeetCode
+//
+//  Created by Rakuyo on 2020/4/10.
+//  Copyright © 2020 com.rakuyo. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+
+    /// 22. 括号生成 中等
+    /// 
+    /// [url](https://leetcode-cn.com/problems/generate-parentheses/)
+    /// 
+    /// 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+    /// 
+    /// - Parameter n:
+    /// - Returns:
+    func generateParenthesis(_ n: Int) -> [String] {
+        
+        var result: [String] = []
+        
+        var left = 0
+        var right = 0
+        
+        backtrack(n, left: &left, right: &right, string: "", result: &result)
+        
+        return result
+    }
+    
+    func backtrack(
+        _ n: Int, 
+        left: inout Int, 
+        right: inout Int, 
+        string: String, 
+        result: inout [String]
+    ) {
+        
+        var string = string
+        
+        if left == n && right == n {
+            result.append(string)
+            return
+        }
+        
+        guard left >= right else { return }
+        
+        let list = left == n ? [")"] : ["(", ")"]
+        
+        for str in list {
+            
+            string.append(str)
+            
+            str == "(" ? (left += 1) : (right += 1)
+            
+            backtrack(n, left: &left, right: &right, string: string, result: &result)
+            
+            string.removeLast()
+            
+            str == "(" ? (left -= 1) : (right -= 1)
+        }
+    }
+    
+    func _22() {
+        
+        print(generateParenthesis(3)) // [ "((()))", "(()())", "(())()", "()(())", "()()()" ]
+    }
+}
