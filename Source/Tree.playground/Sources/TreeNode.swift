ba9b7317f6a0fea27ee18f4f5c52b4b0d06e8c65
Rakuyo <rakuyo.mo@gmail.com>
2020-10-27 16:10:50 +0800

    Improve the implementation of TreeNode

--- Source/Tree.playground/Sources/TreeNode.swift
+++ Source/Tree.playground/Sources/TreeNode.swift
@@ -7,16 +7,28 @@ public class TreeNode {
     public var left: TreeNode?
     public var right: TreeNode?
     
+    public init() {
+        self.val = 0
+        self.left = nil
+        self.right = nil
+    }
+    
     public init(_ val: Int) {
         self.val = val
         self.left = nil
         self.right = nil
+    }
+    
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
     }
 }
 
 extension TreeNode: Equatable {
     
     public static func ==(lhs: TreeNode, rhs: TreeNode) -> Bool {
-        return lhs === rhs
+        return lhs.val == rhs.val && lhs.left == rhs.left && lhs.right == rhs.right
     }
 }
